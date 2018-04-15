2c6c43e2123abd03a1ee25ecc7e439598474f4d2
pedroeveling <38307430+pedroeveling@users.noreply.github.com>
2018-04-15 18:21:12 -0300

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,16 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+class Menu
+{
+    public:
+        Menu();
+        virtual ~Menu();
+    int Cenarios();
+    protected:
+
+    private:
+};
+
+#endif // MENU_H
