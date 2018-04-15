7cea68620cd56426ca08c54f6dc867c21931302f
thomasbsjf <37720233+thomasbsjf@users.noreply.github.com>
2018-04-15 18:38:55 -0300

    Add files via upload

--- TrabalhoED2PT1/include/Sorts.h
+++ TrabalhoED2PT1/include/Sorts.h
@@ -7,29 +7,46 @@ class Sorts
     public:
         Sorts();
         virtual ~Sorts();
+
+        /**Funcoes publicas QuickSort e InsertionSort**/
         void quickSort(int vet[], int, int);
-        void quickSortMediana(int vet[], int, int);
+        void quickSortMedianaTres(int vet[], int, int);
+        void quickSortMedianaCinco(int vet[], int inicio, int fim);
         void quickSortInsertion(int vet[], int, int);
-        void insertionSort(int vet[], int n);
-        void constroiHeap(int vet[], int, int);
-        void heapSort(int vet[], int tam);
-        //void mergeSort(std::vector<int> vet, int inicio, int fim);
-        void mergeSort(int vet[], int inicio, int fim);
         double medianaDeTres(int vet[], int inicio, int fim);
         double medianaDeCinco(int vet[], int inicio, int fim);
+        void insertionSort(int vet[], int n);
+        void manualSortQuick(int vet[], int inicio, int fim);
+        int numeroAleatorio(int menor, int maior);
+
+        /**Funcoes heapSort**/
+        void heapSort(int vet[], int tam);
+
+        /**Funcoes publicas heapSort**/
+        void mergeSort(int vet[], int inicio, int fim);
+
         int getContadorCompara() {return contadorCompara;};
         void addContadorCompara() {contadorCompara += 1;}
         int getNumRegistros() {return numRegistros;};
         void addNumRegistros() {numRegistros += 1;}
         void zeraContadorCompara() {contadorCompara = 0;}
         void zeraNumRegistros() {numRegistros = 0;}
+
+        /**Funcoes impressao de comparacoes e registros**/
         void imprimeValores();
 
     private:
-        // merging(std::vector<int> vet,int,int,int);
+        /**Funcoes privadas mergeSort**/
         void merging(int vet[],int,int,int);
+
+        /**Funcoes privadas heapSort**/
+        void constroiHeap(int vet[], int, int);
+
+        /**Funcoes privadas quickSort heapSort**/
         int quickSepara(int vet[], int, int);
         int quickSeparaMediana(int vet[],int, int, int);
+
+        /**Atributos privados da classe**/
         int contadorCompara;
         int numRegistros;
 };
