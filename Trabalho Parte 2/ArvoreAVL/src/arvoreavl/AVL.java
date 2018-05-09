5e57d4758353489e2f06c182cd8ae28b8fdbf4e1
thomasbsjf <37720233+thomasbsjf@users.noreply.github.com>
2018-05-08 22:27:53 -0300

    Add files via upload

--- Trabalho Parte 2/ArvoreAVL/src/arvoreavl/AVL.java
+++ Trabalho Parte 2/ArvoreAVL/src/arvoreavl/AVL.java
@@ -1,6 +1,16 @@
 package arvoreavl;
 
 public class AVL {
+
+    No raiz;
+    
+    public AVL() {
+        raiz = null;
+    }
+    
+    public No getRaiz() {
+        return raiz;
+    }
     
     public int Altura(No p){
         Integer alturaEsq, alturaDir;
@@ -28,4 +38,101 @@ public class AVL {
             Set_FB(p.getProximo());
         }
     }
+    
+    public No rot_dir(No p){
+        No q = p.getAnterior();
+        p.setAnterior(q.getProximo());
+        q.setProximo(p);
+        
+        return q;
+    }
+    
+    public No rot_esq(No p){
+        No q = p.getProximo();
+        p.setProximo(q.getAnterior());
+        q.setAnterior(p);
+        return q;
+    }
+        
+    public No rot_duplo_dir(No p){
+        p = rot_esq(p.getAnterior());
+        return rot_dir(p);
+    }
+            
+    public No rot_duplo_esq(No p){
+        p = rot_dir(p.getProximo());
+        return rot_esq(p);
+    }
+    
+    public No CorrigeAVL(No p){
+        if(p != null)
+        {
+            p.setMarca(Calcula_FB(p));
+            if(p.getMarca() == 2)
+            {
+                p.getAnterior().setMarca(Calcula_FB(p.getAnterior()));
+                if(p.getAnterior().getMarca() >0)
+                {
+                    p = rot_dir(p);
+                }else
+                {
+                    p = rot_duplo_dir(p);
+                }
+                       
+            }        
+        }
+        return p;
+    }
+    
+    public No InsereAVL(No p, int chave){
+        if(p == null){
+            p = new No(null, null);
+            p.setValor(chave);
+            p.setMarca(0);
+            return p;
+        }else
+        {
+            if(chave < p.getValor()){
+                p.setAnterior(InsereAVL(p.getAnterior(), chave));
+            }else
+            {
+                p.setProximo(InsereAVL(p.getProximo(), chave));
+            }
+        }
+        return p;
+    }
+    
+    public No Consulta(No p, int chave){
+        while(p != null)
+        {
+            if(chave == p.getValor()){
+                return p;
+            }
+            else
+            {
+                if(chave < p.getValor()){
+                    p = p.getAnterior();
+                }
+                else{
+                    p = p.getProximo();
+                }
+            }
+        }
+        return null;
+    }
+    
+    public No RemoveAVL(No p, int n){
+        return p;
+    }
+
+    public No Remove(No p, int n){
+        return p;
+    }
+    
+    void insere(int chave){
+        raiz = InsereAVL(raiz, chave);
+        //raiz = CorrigeAVL(raiz);
+        Set_FB(raiz);
+    }
+    
 }
